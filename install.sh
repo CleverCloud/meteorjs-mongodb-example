e16f922f9c919211f1bf384a90e76b50339af5b7
BlackYoup <a.lefebvre@outlook.fr>
2017-03-24 10:44:15 +0100

    use meteor build

--- install.sh
+++ install.sh
@@ -1,26 +1,8 @@
-#! /bin/bash
+#!/usr/bin/env bash
+set -euf -o pipefail
 
-# Current path
-currentPath=$(realpath ./)
-
-# Install demeteorizer
-cd ~/ && npm install demeteorizer
-
-# Go back to our project
-cd "$currentPath"
-
-# Note: When using "meteorhacks:npm"
-# to prevent the error: "unknown package: npm-container"
-# (described in https://github.com/meteorhacks/npm/issues/49)
-# uncomment the two following lines:
-#meteor remove npm-container
-#meteor run
-
-# demeteorize the app
-~/node_modules/.bin/demeteorizer -o my_app/
-
-# Go inside our demeteorized app to install modules
-cd my_app/bundle/programs/server/
-
-# Install modules
+output="${HOME}/${APP_ID}"-output
+meteor build "${output}" --directory --server-only --architecture os.linux.x86_64
+mv ${output} ./my_app
+cd my_app/bundle/programs/server
 npm install
