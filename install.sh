02fc7fe5c5f5bc896dc2295413ef3555b7483891
BlackYoup <a.lefebvre@outlook.fr>
2017-06-02 14:08:02 +0200

    We now support meteor.js variant

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euf -o pipefail
-
-output="${HOME}/${APP_ID}"-output
-meteor build "${output}" --directory --server-only --architecture os.linux.x86_64
-mv ${output} ./my_app
-cd my_app/bundle/programs/server
-npm install
